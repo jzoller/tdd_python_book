1f980c5fb0f842dba10cb1fb362b258de9a59e91
Jason Zoller <zoller.jason@gmail.com>
2017-04-03 22:15:34 -0500

    fixed functional test

--- superlists/superlists/functional_test.py
+++ superlists/superlists/functional_test.py
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_if('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
@@ -41,7 +41,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peakcock feathers', [row.text for row in rows])
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         # There still a textbox to add another item
         # She enters "Use peacock feathers to make a fly"
         
@@ -50,9 +50,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.assertIn('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
         self.fail('Finish the test!!')
