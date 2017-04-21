23260717a29dca8bfac5e878da41a0e5d13c1c8f
Jason Zoller <zoller.jason@gmail.com>
2017-04-21 02:19:18 -0500

    used bootstrap to improve layout and styling; setup functional test to properly load static files

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         chrome_options = Options()
